bfac5ccff7d6f25c191b4da435d03f8285aadabd
gotik29 <gotik929@gmail.com>
2021-11-10 21:53:59 +0300

    com

---
+++ #src/js/files/script.js
@@ -0,0 +1,11 @@
+const baguetteBox = require("../../../../../../Shared/VSCode/WEB/2021/binary/binary/js/baguetteBox");
+
+if($('.gallery').length>0){
+    baguetteBox.run('.gallery', {
+    });
+}
+
+new Swiper('.swiper__body');
+
+
+
